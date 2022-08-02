6dead4baad2e20eaa7d79ae42f57d01c862d2298
Julián Melero Hidalgo <hola@julianmelero.com>
2022-08-02 09:55:25 +0200

    Strings

---
+++ src/04-strings.ts
@@ -0,0 +1,14 @@
+(() => {
+    /********** Strings ************/
+    let text: string;
+    text = "This is a text 'string' ";
+
+    console.log(text);
+
+    const summary = `
+        title: ${text}
+        price: 500$
+    `;
+
+    console.log(summary);
+})();
