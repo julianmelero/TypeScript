31823c5e434a16e9c480e5a4939e86110c735e38
Julián Melero Hidalgo <hola@julianmelero.com>
2022-08-02 10:05:50 +0200

    Arrays

---
+++ src/05-arrays.ts
@@ -0,0 +1,7 @@
+(() => {
+    /*********** Arrays ************/
+    let pricesAndDesc: (number | string)[];
+    pricesAndDesc = [25, 50 ,75, 'Prorduct 1', 'Product 2', 'Prooduct 3'];
+    
+
+})();
